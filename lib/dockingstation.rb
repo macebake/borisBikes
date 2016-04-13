b830c1f50411ef23dfd445358d9c88d33696dd37
Emilio <emsmnndz@gmail.com>
2016-04-13 11:10:19 +0100

    Dokcing Station releases bikes

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,5 +1,7 @@
+require_relative 'bike'
 class DockingStation
   station = DockingStation.new
   def release_bike
+    Bike.new
   end
 end
---
+++ spec/bike_spec.rb
@@ -0,0 +1,6 @@
+require 'Bike'
+describe Bike do
+  it {expect(subject).to respond_to :working?}
+  #it {is_expected...}
+
+end
