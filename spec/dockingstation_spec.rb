1d4c3a49d03b036da886567fcec0d8d12cfe3749
Macey <macebake@gmail.com>
2016-04-13 13:41:59 +0100

    fixing docking station spec file

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,4 +1,8 @@
 require "dockingstation"
-describe DockingStation do
-  it {expect(subject).to respond_to :release_bike}
-end
+ describe DockingStation do
+   it {expect(subject).to respond_to :release_bike}
+   it 'releases working bike' do
+   newbike = subject.release_bike
+    expect(newbike).to be_working
+  end
+ end
