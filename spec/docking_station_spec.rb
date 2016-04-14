5fcbc5da25d24d41fe92dcc7daec6cbd8a12debc
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-04-14 16:38:37 +0100

    added mocks to tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,18 @@
 require 'docking_station'
 
 describe DockingStation do
-  let(:bike) { Bike.new }
+  let(:bike) { double(:bike)} #refers to another class
 
   describe '#releasebike' do
     it 'releases a bike' do
+      allow(bike).to receive(:working?).and_return(true)
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
 
     it 'does not release a bike if it is broken' do
+      allow(bike).to receive(:report_broken).and_return(true)
+      allow(bike).to receive(:working?).and_return(false)
       bike.report_broken
       subject.dock(bike)
       expect{subject.release_bike}.to raise_error("No working bikes available")
@@ -38,14 +41,14 @@ describe DockingStation do
   describe '#intialize' do
     it "allows user to set capacity when creating a new docking station" do
       dock5 = DockingStation.new(5)
-      5.times {dock5.dock(Bike.new)}
-      expect {dock5.dock(Bike.new)}.to raise_error("Docking Station full")
+      5.times {dock5.dock(double(:bike))}#refers to another class
+      expect {dock5.dock(double(:bike))}.to raise_error("Docking Station full")#refers to another class
     end
 
     it "sets the capacity to DEFAULT_CAPACITY if user does not specify capacity" do
       default_dock = DockingStation.new
-      DockingStation::DEFAULT_CAPACITY.times {default_dock.dock(Bike.new)}
-      expect {default_dock.dock(Bike.new)}.to raise_error("Docking Station full")
+      DockingStation::DEFAULT_CAPACITY.times {default_dock.dock(double(:bike))}#refers to another class
+      expect {default_dock.dock(double(:bike))}.to raise_error("Docking Station full")#refers to another class
     end
   end
 end
