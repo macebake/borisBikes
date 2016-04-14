ae52a92faba191df5783fef61c739d8aa29f840b
Macey <macebake@gmail.com>
2016-04-14 10:23:23 +0100

    Adding full & empty methods

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -8,14 +8,21 @@ class DockingStation
     @bikes = 0
   end
 
-def release_bike
-    raise "no bikes available" if @bikes == 0
-    @bikes -= 1
+  def release_bike
+    empty? ? (raise "no bikes available") : (@bikes -= 1)
   end
 
   def dock(bike)
-    raise "no docks available" if @bikes >= 20
-    @bikes += 1
-    end
+    full? ? (raise "no docks available") : (@bikes += 1)
+  end
+
+  private
+  def empty?
+    (@bikes == 0)? true : false
+  end
+
+  def full?
+    (@bikes >= 20)? true : false
+  end
 
 end
