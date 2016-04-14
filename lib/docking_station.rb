855fae64a9612a0eec886afcaaac3e5365902f0e
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-04-14 10:36:39 +0100

    created full? and empty? predicate methods and made them private. These methods have been feature tested

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,13 +9,23 @@ class DockingStation
   end
 
   def release_bike
-    fail "No bikes" if @bikes.empty?
+    fail "No bikes" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail "Docking Station full" if @bikes.count >= 20
+    fail "Docking Station full" if full?
     @bikes << bike
+  end
+
+private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
