f19ad532ae4606ee73dbfbaf9e270b4b5c48ff59
Matthew Ward <matthewward@Matthews-MacBook-Pro-3.local>
2016-04-14 10:00:57 +0100

    Refactored tests and docking_station.rb

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,21 +1,21 @@
 require_relative 'bike'
 
 class DockingStation
-	attr_reader :bikes
 
-	def initialize
-		@bikes = []
-	end
+  attr_reader :bikes
 
-def release_bike
-		fail "No bikes" if @bikes.empty?
-		@bikes.pop
+  def initialize
+    @bikes = []
+  end
 
-end
+  def release_bike
+    fail "No bikes" if @bikes.empty?
+    @bikes.pop
+  end
 
-def dock(bike)
-	fail "Docking Station full" if @bikes.count >= 20
-	@bikes << bike
-end
+  def dock(bike)
+    fail "Docking Station full" if @bikes.count >= 20
+    @bikes << bike
+  end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,37 +1,33 @@
 require 'docking_station'
 
 describe DockingStation do
-	it {is_expected.to respond_to :release_bike}
-	it {is_expected.to respond_to(:dock).with(1).argument }
-	it {is_expected.to respond_to (:bikes)}
+  let(:bike) { Bike.new }
 
-	describe '#releasebike' do
-		it 'releases a bike' do
-			bike = Bike.new
-			subject.dock(bike)
-			expect(subject.release_bike).to eq bike
-		end
-	end
+  describe '#releasebike' do
+    it 'releases a bike' do
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+  end
 
-	it "has a working bike" do
-		bike = Bike.new
-		subject.dock(bike)
-		expect(subject.release_bike.working?).to eq true
-	end
+  it "has a working bike" do
+    subject.dock(bike)
+    expect(subject.release_bike.working?).to eq true
+  end
 
-it "return docked bikes" do
-	bike = Bike.new
-	subject.dock(bike)
-	expect(subject.bikes).to eq bike
-end
+  it "return docked bikes" do
+    subject.dock(bike)
+    expect(subject.bikes[0]).to eq bike
+  end
 
-it "raises exception if no bikes in docking station" do
-	expect {subject.release_bike}.to raise_error("No bikes")
-end
-describe '#dock' do
-it "raises exception if docking station is full" do
-	20.times {subject.dock(Bike.new)}
-	expect {subject.dock(Bike.new)}.to raise_error("Docking Station full")
-end
-end
+  it "raises exception if no bikes in docking station" do
+    expect {subject.release_bike}.to raise_error("No bikes")
+  end
+
+  describe '#dock' do
+    it "raises exception if docking station is full" do
+      20.times {subject.dock(Bike.new)}
+      expect {subject.dock(Bike.new)}.to raise_error("Docking Station full")
+    end
+  end
 end
