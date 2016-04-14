e36c18eb7111345fe02b107e86991f1833808c7d
Matthew Ward <matthewward@Matthews-MacBook-Pro-3.local>
2016-04-14 10:53:04 +0100

    Removed all instances of magic numbers and replaced with constant value

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -26,7 +26,7 @@ describe DockingStation do
 
   describe '#dock' do
     it "raises exception if docking station is full" do
-      20.times {subject.dock(Bike.new)}
+      DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
       expect {subject.dock(Bike.new)}.to raise_error("Docking Station full")
     end
   end
