adb09bc87fb2a8163fac7a1ebf51db4faba7c0df
Macey <macebake@gmail.com>
2016-04-15 14:58:57 +0100

    Van raises error if there are no broken bikes - test fails

--- spec/ven_spec.rb
+++ spec/ven_spec.rb
@@ -12,8 +12,16 @@ describe Van do
   end
 
   it "raises exception if no bikes in docking station" do
-    station = DockingStation.new
+  station = DockingStation.new
   expect {subject.pickup_broken_bikes(station)}.to raise_error("No bikes")
+  end
+
+  it "raises an exception if bikes are working" do
+  station = DockingStation.new
+  bike = Bike.new
+  bike.report_broken
+  station.dock(bike)
+  expect {subject.pickup_broken_bikes(station)}.to raise_error("No broken bikes to pick up")
   end
 
 end
